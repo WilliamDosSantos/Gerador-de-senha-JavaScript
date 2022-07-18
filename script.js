ac6a7d4ba13d1d54dbf1ed93db59a78118aa2ee2
William Lucas <williamsantos1506@gmail.com>
2022-07-18 19:23:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const passInput = document.querySelector("#inputPasswordId");
+const lenInput = document.querySelector("#inputLengthId");
+const infoLength = document.querySelector("#labelLengthId");
+const btnGerar = document.querySelector("#btnGerar");
+
+const chkLower = document.querySelector("#chkLowerId");
+const chkUpper = document.querySelector("#chkUpperId");
+const chkNumber = document.querySelector("#chkNumberId");
+const chkSymbols = document.querySelector("#chkSymbolsId");
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = ["!", "@", "#", "$", "%"];
+
+const caracters = Array.from(Array(26)).map((_, i) => i + 97);
+const LowercaseCaracters = caracters.map((item) => String.fromCharCode(item));
+const UppercaseCaracters = LowercaseCaracters.map((item) => item.toUpperCase());
+
+infoLength.innerHTML = lenInput.value;
+
+lenInput.addEventListener("change", () => {
+  infoLength.innerHTML = lenInput.value;
+});
+
+btnGerar.addEventListener("click", () => {
+  generatePassword(
+    chkNumber.checked,
+    chkSymbols.checked,
+    chkLower.checked,
+    chkUpper.checked,
+    lenInput.value
+  );
+});
+
+const generatePassword = (
+  hasNumbers,
+  hasSymbols,
+  hasLowercase,
+  hasUppercase,
+  lenght
+) => {
+  const newArray = [
+    ...(hasNumbers ? numbers : []),
+    ...(hasSymbols ? symbols : []),
+    ...(hasLowercase ? LowercaseCaracters : []),
+    ...(hasUppercase ? UppercaseCaracters : []),
+  ];
+
+  if (newArray.length === 0) return;
+
+  let password = "";
+
+  for (let i = 0; i < lenght; i++) {
+    const randomIndex = Math.floor(Math.random() * newArray.length);
+    password += newArray[randomIndex];
+  }
+
+  passInput.value = password;
+};
